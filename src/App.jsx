b4b1fb7793a138d7145abd4421db6226a6d162d4
Francesco <francesco.clementi@mail-bip.com>
2021-01-13 13:54:22 +0100

    Refactored components interaction

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState, useEffect } from 'react';
 import { HeaderBar } from "./components/HeaderBar";
 import { FooterBar } from "./components/FooterBar";
 import { MapArea } from "./components/MapArea";
-import { MapAreaByCat } from "./components/MapAreaByCat";
 import { MapAreaByDeliveryLocation } from "./components/MapAreaByDeliveryLocation";
 import { StaticBlock } from "./components/StaticBlock";
 import { LocationsTable } from "./components/LocationsTable";
@@ -11,180 +10,149 @@ import { Total } from "./components/Total";
 import { loadData } from "./loadData";
 import { BarChart } from "./components/BarChart";
 import { HBarChart } from "./components/HBarChart";
-import { areaMappingReverse, groupByAge, allTotalGender, hideLoader } from "./utils";
-import * as _ from 'lodash';
+import { hideLoader } from "./utils";
 import "./App.css";
-import { omit } from "lodash";
+import { find, head, max } from "lodash";
 
 
 function App() {
   const [summary, setSummary] = useState({});
-  const [selected, setSelected] = useState(null);
+  const [deliveryTableData, setDeliveryTableData] = useState([]);
+  const [deliverySelectedRegion, setDeliverySelectedRegion] = useState(null);
+  const [categorySelectedRegion, setCategorySelectedRegion] = useState(null);
+  const [categoryData, setCategoryData] = useState([]);
+  const [categoryMapData, setCategoryMapData] = useState([]);
+  const [totalDelivery, setTotalDelivery] = useState(0);
   const [totalAgeByGender, setTotalAgeByGender] = useState({});
-  const [barState, setBarState] = useState([]);
+  const [deliveryBarChartData, setdeliveryBarChartDataData] = useState([]);
   const [selectedLocation, setSelectedLocation] = useState("");
-  const [selectedLocationMap, setSelectedLocationMap] = useState(null);
-  const [selectedLocationCategoryMap, setSelectedLocationCategoryMap] = useState(null);
   const [selectedAge, setSelectedAge] = useState(null);
-  const [selectedCategory, setSelectedCategory] = useState([]);
-  const [selectedFilterByAge, setSelectedFilterByAge] = useState(null);
   const [selectedCodeCategory, setSelectedCodeCategory] = useState(null);
-  const [categoryRegionSelect, setCategoryRegionSelect] = useState(null);
   const [totalByCategory, setTotalByCategory] = useState(0);
-  const [maxByCategory, setMaxByCategory] = useState(0);
   const [locationTableRef, setLocationTableRef] = useState(0);
   const [locationRegionSelect, setLocationRegionSelect] = useState(null);
-
-  const simulateClick = (id) => {
-    if (document.getElementById(id) && id) {
-      let clickEvt = new MouseEvent('click', {
-        'bubbles': true,
-        'cancelable': true
-      });
-      document.getElementById(id).dispatchEvent(clickEvt);
-    }
-  }
-
-  const resetFilter = () => {
-    simulateClick(selected?.area);
-    simulateClick(selectedAge?.fascia_anagrafica)
-    setSelected(null);
-    setSelectedCategory(summary.categories);
-    setSelectedCodeCategory(null);
-    setSelectedLocationCategoryMap(null);
-    setCategoryRegionSelect(null);
-    setSelectedLocation(null);
-    setLocationRegionSelect(null);
-  }
-
-  const loadRect = (rect) => {
-    setSelectedAge(rect)
-    setTotalAgeByGender({ gen_m: rect?.sesso_maschile, gen_f: rect?.sesso_femminile });
-  }
-
-  const setTableFilteredVaccini = (currentRect) => {
-    let vaccinAdministrationListReportByAge = summary.dataSomeVaxDetail.filter(el => (el.fascia_anagrafica.trim()) === (currentRect.fascia_anagrafica.trim()));
-    var grouped = _.mapValues(_.groupBy(vaccinAdministrationListReportByAge, 'area'),
-      z => _.sum(z.map(x => _.sum([x.sesso_maschile, x.sesso_femminile]))));
-    let _summary = summary.deliverySummary;
-    _summary = _summary.map((e) => {
-      let x = omit(e, ['dosi_somministrate', 'percentuale_somministrazione', 'ultimo_aggiornamento']);
-      let y = { dosi_somministrate: grouped[e.area] };
-      let z = { percentuale_somministrazione: ((y.dosi_somministrate / x.dosi_consegnate) * 100).toFixed(1) }
-      return { ...x, ...y, ...z };
-    });
-    setSelectedFilterByAge(_summary);
-  }
-  const handleRectClick = (currentRect) => {
-    let currentRectExist = currentRect ? true : false;
-    let selectedExist = selected ? true : false;
-    if (!currentRectExist) {
-      setBarState(summary.categoriesAndAges);
-      setTotalAgeByGender(summary.gender);
-      setSelectedFilterByAge(null);
-      setSelectedAge(null)
-      setSelected(null);
-    }
-    if (currentRectExist && selectedExist) {
-      resetFilter();
-      let currentRectDefault = summary?.categoriesAndAges.filter((e) => e?.fascia_anagrafica === currentRect?.fascia_anagrafica);
-      setTableFilteredVaccini(currentRect);
-      loadRect(currentRectDefault[0])
-    }
-    if (currentRectExist && !selectedExist) {
-      setTableFilteredVaccini(currentRect);
-      loadRect(currentRect);
-    }
-  }
-  const handleHRectClick = (currentRect) => {
-
-    if (currentRect) {
-      setSelectedCodeCategory(currentRect?.code)
-    } else {
-      setSelectedCodeCategory(null)
-    }
-  }
-
-  const handleCountryClick = (countryIndex) => {
-    let _selected = summary.deliverySummary[countryIndex];
-
-    setSelected({ ..._selected });
-    setSelectedAge(null);
-    setSelectedFilterByAge(null);
-    setSelectedLocationMap(_selected);
-
-    if (countryIndex || countryIndex === 0) {
-      let vaccinAdministrationListReportByArea = summary.dataSomeVaxDetail.filter(el => el?.area?.trim() === _selected.area?.trim());
-      setBarState(groupByAge(vaccinAdministrationListReportByArea));
-      setTotalAgeByGender(allTotalGender(groupByAge(vaccinAdministrationListReportByArea)));
-    } else {
-      setBarState(summary.categoriesAndAges);
-      setTotalAgeByGender(summary.gender);
-    }
-
-  };
-
-  const handleCountryClickLocations = (countryIndex) => {
-    setSelectedLocation(countryIndex);
-  };
-
-  const handleCountryClickCategories = (countryIndex) => {
-    setSelectedCodeCategory(null)
-    const area = summary.deliverySummary[countryIndex]?.area;
-    const areaCode = areaMappingReverse[area];
-    const data = summary.categoriesByRegions[areaCode];
-
-    let _selected = summary.deliverySummary[countryIndex];
-
-    setSelectedLocationCategoryMap(_selected);
-
-    setTotalByCategory(
-      countryIndex ? _selected.dosi_somministrate : summary.tot
-    )
-    setSelectedCategory(
-      countryIndex ? data?.slice() || [] : summary.categories
-    );
-  };
+  const [categoryMapField, setCategoryMapField] = useState('dosi_somministrate');
 
   useEffect(() => {
     loadData().then((d) => {
       hideLoader();
       setSummary(d);
-      setSelectedCategory(d.categories);
-      setBarState(d.categoriesAndAges);
+      setdeliveryBarChartDataData(d.categoriesAndAges);
       setTotalAgeByGender(d.gender);
-      setSelectedFilterByAge(null);
+      setDeliveryTableData(d.totalDeliverySummary);
+      setTotalDelivery(d.tot);
+      setCategoryData(d.categories);
+      setTotalByCategory(d.tot);
+      setCategoryMapData(d?.totalDeliverySummary)
     });
   }, []);
 
-  useEffect(() => {
-    let totalSumm = 0;
-    let maxSumm = 0;
+  const resetFilter = () => {
+    setDeliverySelectedRegion(null);
+    setDeliveryTableData(summary.totalDeliverySummary);
+    setTotalDelivery(summary.tot);
+    setTotalAgeByGender(summary.gender)
+    setdeliveryBarChartDataData(summary.categoriesAndAges)
+    setSelectedCodeCategory(null);
+    setSelectedLocation(null);
+    setLocationRegionSelect(null);
+  }
 
-    if (selectedCodeCategory) {
-      setSelectedLocationCategoryMap(null)
+  const handleCountryClickLocations = (countryIndex) => {
+    setSelectedLocation(countryIndex);
+  };
+
+  const fillMapDeliveryArea = ({region, maxValue, field}) => {
+    let scaleOp = 0
+    if(region.code === deliverySelectedRegion){
+       scaleOp = 1 
+    }else if(!deliverySelectedRegion){
+      scaleOp = max([region[field]/maxValue,0.1])
+    }else{
+      const valueToFill = region[field] / 2
+      scaleOp = max([valueToFill, 0.1])
     }
-    if (!selectedLocationCategoryMap) {
-      summary?.deliverySummary?.forEach(i => {
-        Object.keys(i.byCategory).forEach(cat => {
-          if (!selectedCodeCategory) {
-            totalSumm = totalSumm + (i.byCategory[cat].length && i.byCategory[cat][0].total) || 0
-            maxSumm = (i.byCategory[cat].length && i.byCategory[cat][0].total) > maxSumm ?
-              (i.byCategory[cat].length && i.byCategory[cat][0].total) : maxSumm
+    return `rgba(0,102,204,${scaleOp}) `
+  }
 
-          } else if (selectedCodeCategory && cat === selectedCodeCategory) {
-            totalSumm = totalSumm + (i.byCategory[cat].length && i.byCategory[cat][0].total) || 0
-            maxSumm = (i.byCategory[cat].length && i.byCategory[cat][0].total) > maxSumm ?
-              (i.byCategory[cat].length && i.byCategory[cat][0].total) : maxSumm
-          }
-        })
-      });
+  const handleMapDeliveryClick = (region) => {
 
-      setMaxByCategory(maxSumm)
-      setTotalByCategory(totalSumm)
+    if(deliverySelectedRegion === region.code){
+      setDeliverySelectedRegion(null);
+      setDeliveryTableData(summary.totalDeliverySummary);
+      setTotalDelivery(summary.tot);
+      setTotalAgeByGender(summary.gender)
+      setdeliveryBarChartDataData(summary.categoriesAndAges)
+    }else{
+      const deliveryTableFilteredData = find(deliveryTableData, d => d.code === region.code);
+
+      setDeliveryTableData([deliveryTableFilteredData])
+      setdeliveryBarChartDataData(deliveryTableFilteredData.byAge)
+      setDeliverySelectedRegion(region.code)
+      setTotalDelivery(region.dosi_somministrate)
+      setTotalAgeByGender({ gen_m: deliveryTableFilteredData.sesso_maschile, gen_f: deliveryTableFilteredData.sesso_femminile });
     }
-    // eslint-disable-next-line
-  }, [selectedCodeCategory, summary, totalByCategory])
+  }
+
+  const handleDeliveryBarChartClick = (data) => {
+    setDeliverySelectedRegion(null);
+
+    if(data.fascia_anagrafica === selectedAge){
+      setSelectedAge(null);
+      setDeliveryTableData(summary.totalDeliverySummary);
+      setTotalAgeByGender(summary.gender)
+      setTotalDelivery(summary.tot)
+    }else{
+      setTotalDelivery(data.totale)
+      setSelectedAge(data.fascia_anagrafica);
+      setDeliveryTableData(head(summary.totalDeliverySummaryByAge[data.fascia_anagrafica]).details);
+      setTotalAgeByGender({ gen_m: data.sesso_maschile, gen_f: data.sesso_femminile });
+    }    
+
+  }
+
+  const resetCategoryFilter = () => {
+    setCategorySelectedRegion(null)
+    setCategoryData(summary.categories);
+    setTotalByCategory(summary.tot)
+    setSelectedCodeCategory(null)
+    setCategoryMapField('dosi_somministrate')
+  }
+
+  const handleMapCategoryClick = (region) => {
+    
+    if(categorySelectedRegion === region.code){
+      resetCategoryFilter()
+    }else{
+      setCategorySelectedRegion(region.code)
+      setCategoryData(summary.categoriesByRegions[region.code]);
+      setTotalByCategory(region.dosi_somministrate)
+    }
+  }
+
+  const fillMapCategoryArea = ({region, maxValue, field}) => {
+    let scaleOp = 0
+    if(region.code === categorySelectedRegion){
+       scaleOp = 1 
+    }else if(!categorySelectedRegion){
+      scaleOp = max([region[field]/maxValue,0.1])
+    }else{
+      const valueToFill = region[field] / (2*maxValue)
+      scaleOp = max([valueToFill, 0.1])
+    }
+    return `rgba(0,102,204,${scaleOp}) `
+  }
+
+  const handleCategoryBarChartClick = (cat) => {
+
+    if (cat) {
+      setSelectedCodeCategory(cat.code)
+      setCategoryMapField(cat.code)
+      setTotalByCategory(cat.total)
+    } else {
+      resetCategoryFilter()
+    }
+  }
 
   return (
     <div>
@@ -192,9 +160,7 @@ function App() {
       <div className="container">
         <div className="row">
           <div className="col-12 d-flex justify-content-center">
-
             <Total className="mb-3" summary={{ ...summary }} />
-
           </div>
         </div>
         <div className="row">
@@ -235,55 +201,41 @@ function App() {
               <span data-toggle="tooltip" title="% somministrazioni su dosi consegnate" className="circle-info">i</span>
             </div>
             <Table
-              summaryFilter={selectedFilterByAge}
-              summary={{ ...summary }}
-              selected={selected}
+              deliveryTableData={deliveryTableData}
               className="mr-5 h-100"
             />
           </div>
           <div className="col-12 col-lg-7 order-md-1 order-lg-2">
             <div className="p-4 position-relative d-lg-none">
-
               <div className="w-100 h-100 d-flex justify-content-start pr-5">
                 <img src="logo.png" width="35" height="35" alt="Logo" />
-
                 <h5 className="pl-3 pl-sm-1">Distribuzione vaccinazioni<br /> rispetto alle consegne</h5>
-
               </div>
-
-
             </div>
             <div className="p-4 position-relative d-none d-lg-block" style={{ left: '300px', top: '190px' }}>
-
               <div className="w-100 h-100 d-flex justify-content-start pr-5">
                 <img src="logo.png" width="35" height="35" alt="Logo" />
-
                 <h5 className="pl-3 pl-sm-1">Distribuzione vaccinazioni<br /> rispetto alle consegne</h5>
-
               </div>
-
-
             </div>
             <MapArea
-              summaryFilter={selectedFilterByAge}
-              summary={{ ...summary }}
-              selected={selectedLocationMap}
-              handleCountryClick={handleCountryClick}
+              fillMapDeliveryArea={fillMapDeliveryArea}
+              summary={deliveryTableData}
+              handleMapDeliveryClick={handleMapDeliveryClick}
+              tooltip={(r)=>r.area + " " + (r.percentuale_somministrazione && r.percentuale_somministrazione.toLocaleString('it')) + "%"}
+              fillBy="percentuale_somministrazione"
               className="ml-5 w-100 h-100"
             />
             <div className="p-4 position-relative">
               <div className="text-black w-100">
                 <div className="w-100 h-100 d-flex justify-content-start ">
                   <img src="logo.png" width="45" height="45" alt="Logo" className="mt-3" />
-                  <span className="font-50 pl-3" >{_.sum([totalAgeByGender?.gen_m, totalAgeByGender?.gen_f]) ? _.sum([totalAgeByGender?.gen_m, totalAgeByGender?.gen_f]).toLocaleString('it') : 0}</span>
-
+                  <span className="font-50 pl-3" >{totalDelivery.toLocaleString('it')}</span>
                 </div>
-
                 <div className="w-100  h-100 d-flex justify-content-start">
                   <h5>Totale vaccinazioni</h5>
                 </div>
               </div>
-
             </div>
           </div>
         </div>
@@ -319,10 +271,8 @@ function App() {
                 </div>
                 <div className="w-100  h-100 d-flex justify-content-end text-right">
                   <h3>Vaccinazioni<br></br> per fasce di età</h3>
-
                 </div>
               </div>
-
             </div>
           </div>
           <div className="col-12  col-md-7" style={{
@@ -332,12 +282,12 @@ function App() {
               title=""
               xtitle="Fascia d'età"
               ytitle=""
-              width="800"
-              handleRectClick={handleRectClick}
-              height="300"
+              width={800}
+              handleDeliveryBarChartClick={handleDeliveryBarChartClick}
+              height={300}
               selected={selectedAge}
               property={{ xprop: "fascia_anagrafica", yprop: "totale" }}
-              data={[...barState]}
+              data={[...deliveryBarChartData]}
             />
           </div>
         </div>
@@ -353,20 +303,17 @@ function App() {
             <div className="mb-5  d-lg-none " style={{
               position: 'relative',
               background: '#013366',
-
             }}>
               <div className="text-white w-100">
                 <div className="w-100  h-100 d-flex justify-content-start pt-5 pl-4">
                   <h5>Totale<br></br>vaccinazioni</h5>
                 </div>
                 <div className="w-100  h-100 d-flex justify-content-start pl-4">
-                  <p className="numeri_box">{(!selectedCodeCategory && !selectedLocationCategoryMap)
-                    ? summary.tot?.toLocaleString('it')
-                    : totalByCategory?.toLocaleString('it')}
+                <p className="numeri_box">{totalByCategory && totalByCategory.toLocaleString('it')}
                   </p>
                 </div>
                 <div className="col-12 d-flex justify-content-end  pb-2">
-                  <img alt="reset-plot2" src="reset_white.png" onClick={resetFilter} height={35} />
+                  <img alt="reset-plot2" src="reset_white.png" onClick={resetCategoryFilter} height={35} />
                 </div>
               </div>
             </div>
@@ -385,13 +332,11 @@ function App() {
                     <h5>Totale<br></br>vaccinazioni</h5>
                   </div>
                   <div className="w-100  h-100 d-flex justify-content-start pl-4">
-                    <p className="numeri_box">{(!selectedCodeCategory && !selectedLocationCategoryMap)
-                      ? summary.tot?.toLocaleString('it')
-                      : totalByCategory?.toLocaleString('it')}
+                  <p className="numeri_box">{totalByCategory && totalByCategory.toLocaleString('it')}
                     </p>
                   </div>
                   <div className="col-12 d-flex justify-content-end  pb-2">
-                    <img alt="Reset" src="reset_white.png" onClick={resetFilter} height={35} />
+                    <img alt="Reset" src="reset_white.png" onClick={resetCategoryFilter} height={35} />
                   </div>
                 </div>
               </div>
@@ -402,12 +347,12 @@ function App() {
             <HBarChart
               title=""
               xtitle="Vaccinazioni per categoria"
-              handleRectClick={handleHRectClick}
+              handleRectClick={handleCategoryBarChartClick}
               ytitle=""
               width="220"
               height="260"
               property={{ xprop: "name", yprop: "total" }}
-              data={selectedCategory?.slice() || []}
+              data={categoryData}
               selectedCodeCategory={selectedCodeCategory}
             />
           </div>
@@ -424,15 +369,13 @@ function App() {
                 <h5 className="pl-3 pl-sm-1">Vaccinazioni<br /> per regione</h5>
               </div>
             </div>
-            <MapAreaByCat
-              summary={{ ...summary }}
-              selected={selectedLocationCategoryMap}
-              handleCountryClick={handleCountryClickCategories}
-              maxByCategory={maxByCategory}
-              selectedCodeCategory={selectedCodeCategory}
-              setCategoryRegionSelect={setCategoryRegionSelect}
-              categoryRegionSelect={categoryRegionSelect}
-              className="w-100 h-100"
+            <MapArea
+              fillMapDeliveryArea={fillMapCategoryArea}
+              summary={categoryMapData}
+              handleMapDeliveryClick={handleMapCategoryClick}
+              fillBy={categoryMapField}
+              tooltip={(r)=>r.area + " " + (r[categoryMapField] && r[categoryMapField].toLocaleString('it'))}
+              className="ml-5 w-100 h-100"
             />
           </div>
         </div>
